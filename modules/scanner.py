142097037d8d0b5d21d5d140f2187b0034969ef2
srdmsr12345-source <srdmsr12345@gmail.com>
2026-06-27 19:38:51 +0530

    Update scanner.py

--- modules/scanner.py
+++ modules/scanner.py
@@ -1,119 +1,47 @@
-import pandas as pd
+def run(
+    self,
+    scan_type,
+    stock_df,
+    index_df=None
+):
 
-from plugins.bottom import bottom_scanner
-from plugins.volume import volume_scanner
-from plugins.smartmoney import smartmoney_scanner
-from plugins.stage2 import stage2_scanner
-from plugins.orb import orb_scanner
-from plugins.relativestrength import relative_strength_scanner
+    scan_type = scan_type.lower()
 
+    if scan_type == "bottom":
 
-class ScannerEngine:
+        return self.bottom(stock_df)
 
-    def __init__(self):
-        pass
+    elif scan_type == "volume":
 
+        return self.volume(stock_df)
 
-    def bottom(self, df):
+    elif scan_type == "smartmoney":
 
-        return bottom_scanner.top_candidates(df)
+        return self.smartmoney(stock_df)
 
+    elif scan_type == "stage2":
 
-    def volume(self, df):
+        return self.stage2(stock_df)
 
-        return volume_scanner.top_candidates(df)
+    elif scan_type == "orb":
 
+        return self.orb(stock_df)
 
-    def smartmoney(self, df):
+    elif scan_type == "relativestrength":
 
-        return smartmoney_scanner.top_candidates(df)
-
-
-    def stage2(self, df):
-
-        return stage2_scanner.top_candidates(df)
-
-
-    def orb(self, df):
-
-        return orb_scanner.top_candidates(df)
-
-
-    def relativestrength(
-
-        self,
-
-        stock_df,
-
-        index_df
-
-    ):
-
-        return relative_strength_scanner.top_candidates(
-
-            stock_df,
-
-            index_df
-
-        )
-
-
-    def run(
-
-        self,
-
-        scan_type,
-
-        stock_df,
-
-        index_df=None
-
-    ):
-
-        scan_type = scan_type.lower()
-
-        if scan_type == "bottom":
-
-            return self.bottom(stock_df)
-
-        elif scan_type == "volume":
-
-            return self.volume(stock_df)
-                  elif scan_type == "smartmoney":
-
-            return self.smartmoney(stock_df)
-
-        elif scan_type == "stage2":
-
-            return self.stage2(stock_df)
-
-        elif scan_type == "orb":
-
-            return self.orb(stock_df)
-
-        elif scan_type == "relativestrength":
-
-            if index_df is None:
-
-                raise ValueError(
-                    "index_df is required for Relative Strength scan"
-                )
-
-            return self.relativestrength(
-
-                stock_df,
-
-                index_df
-
-            )
-
-        else:
+        if index_df is None:
 
             raise ValueError(
-
-                f"Unknown scan type: {scan_type}"
-
+                "index_df is required for Relative Strength scan"
             )
 
+        return self.relativestrength(
+            stock_df,
+            index_df
+        )
 
-scanner = ScannerEngine()
+    else:
+
+        raise ValueError(
+            f"Unknown scan type: {scan_type}"
+            )
