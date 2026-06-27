701248fc72204f9f0d74d67b2f421a87f923aef4
srdmsr12345-source <srdmsr12345@gmail.com>
2026-06-27 20:14:37 +0530

    Update scanner.py

--- modules/scanner.py
+++ modules/scanner.py
@@ -1,9 +1,40 @@
-    def run(
-        self,
-        scan_type,
-        stock_df,
-        index_df=None
-    ):
+import pandas as pd
+
+from plugins.bottom import bottom_scanner
+from plugins.volume import volume_scanner
+from plugins.smartmoney import smartmoney_scanner
+from plugins.stage2 import stage2_scanner
+from plugins.orb import orb_scanner
+from plugins.relativestrength import relative_strength_scanner
+
+
+class ScannerEngine:
+
+    def __init__(self):
+        pass
+
+    def bottom(self, df):
+        return bottom_scanner.top_candidates(df)
+
+    def volume(self, df):
+        return volume_scanner.top_candidates(df)
+
+    def smartmoney(self, df):
+        return smartmoney_scanner.top_candidates(df)
+
+    def stage2(self, df):
+        return stage2_scanner.top_candidates(df)
+
+    def orb(self, df):
+        return orb_scanner.top_candidates(df)
+
+    def relativestrength(self, stock_df, index_df):
+        return relative_strength_scanner.top_candidates(
+            stock_df,
+            index_df
+        )
+
+    def run(self, scan_type, stock_df, index_df=None):
 
         scan_type = scan_type.lower()
 
@@ -23,6 +54,7 @@
             return self.orb(stock_df)
 
         elif scan_type == "relativestrength":
+
             if index_df is None:
                 raise ValueError(
                     "index_df is required for Relative Strength scan"
