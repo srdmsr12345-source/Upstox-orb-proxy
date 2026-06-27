bc37dc1b84a5d4553b6d36ad0c21567b97eaff61
srdmsr12345-source <srdmsr12345@gmail.com>
2026-06-27 20:09:41 +0530

    Update scanner.py

--- modules/scanner.py
+++ modules/scanner.py
@@ -1,47 +1,42 @@
-def run(
-    self,
-    scan_type,
-    stock_df,
-    index_df=None
-):
+    def run(
+        self,
+        scan_type,
+        stock_df,
+        index_df=None
+    ):
 
-    scan_type = scan_type.lower()
+        scan_type = scan_type.lower()
 
-    if scan_type == "bottom":
+        if scan_type == "bottom":
+            return self.bottom(stock_df)
 
-        return self.bottom(stock_df)
+        elif scan_type == "volume":
+            return self.volume(stock_df)
 
-    elif scan_type == "volume":
+        elif scan_type == "smartmoney":
+            return self.smartmoney(stock_df)
 
-        return self.volume(stock_df)
+        elif scan_type == "stage2":
+            return self.stage2(stock_df)
 
-    elif scan_type == "smartmoney":
+        elif scan_type == "orb":
+            return self.orb(stock_df)
 
-        return self.smartmoney(stock_df)
+        elif scan_type == "relativestrength":
+            if index_df is None:
+                raise ValueError(
+                    "index_df is required for Relative Strength scan"
+                )
 
-    elif scan_type == "stage2":
+            return self.relativestrength(
+                stock_df,
+                index_df
+            )
 
-        return self.stage2(stock_df)
-
-    elif scan_type == "orb":
-
-        return self.orb(stock_df)
-
-    elif scan_type == "relativestrength":
-
-        if index_df is None:
-
+        else:
             raise ValueError(
-                "index_df is required for Relative Strength scan"
+                f"Unknown scan type: {scan_type}"
             )
 
-        return self.relativestrength(
-            stock_df,
-            index_df
-        )
 
-    else:
-
-        raise ValueError(
-            f"Unknown scan type: {scan_type}"
-            )
+scanner = ScannerEngine()
